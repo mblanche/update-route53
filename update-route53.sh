cf066242e5621c4c4504394f9cea27b28627b7bc
Marco Blanchette <marcoblanchette@icloud.com>
2023-11-07 18:28:22 +0000

    adding shebang

--- update-route53.sh
+++ update-route53.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 1800")
 
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)
